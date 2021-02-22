ef22fdfd23bb4e5de26116dda4d3c06ed9ba4fe1
P15166 <john.parask.p15166@gmail.com>
2021-02-22 21:05:46 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     resources :todos do
     resources :items
   end
-
+   post 'signup', to: 'users#create'
    post 'auth/login', to: 'authentication#authenticate'
-    post 'signup', to: 'users#create'
 end
